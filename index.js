5fdf2c38f1b116991e531f9894179bbce3202fc3
apreg <x3cut0r@mailbox.hu>
2015-03-19 05:58:21 +0100

    fixes path issues on windows platform #1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var join = require('path').join;
+var path_ = require('path');
 var resolve = require('resolve');
 var extend = require('lodash').extend;
 var postcss = require('postcss');
@@ -26,86 +26,85 @@ var dirname = require('path').dirname;
  * same for custom media queries.
  */
 function makeVarMap(filename) {
-  var map = {vars: {}, media: {}};
+    var map = {vars: {}, media: {}};
 
-  function resolveImport(path, basedir) {
-    if (path[0] === '/')
-      return path;
+    function resolveImport(path, basedir) {
+        if (path[0] === '/')
+            return path;
 
-    if (path[0] === '.')
-      return join(basedir, path);
+        if (path[0] === '.')
+            return path_.join(path_.dirname(basedir), path);
 
-    // webpack treats anything starting w/ ~ as a module name, which we're
-    // about to do below, so just remove leading tildes
-    path = path.replace(/^~/, '');
+        // webpack treats anything starting w/ ~ as a module name, which we're
+        // about to do below, so just remove leading tildes
+        path = path.replace(/^~/, '');
 
-    return resolve.sync(path, {
-      basedir: basedir,
-      packageFilter: function(package) {
-        var newPackage = extend({}, package);
+        return resolve.sync(path, {
+            basedir: basedir,
+            packageFilter: function(package) {
+                var newPackage = extend({}, package);
 
-        if (newPackage.style != null)
-          newPackage.main = newPackage.style;
+                if (newPackage.style != null)
+                    newPackage.main = newPackage.style;
 
-        return newPackage;
-      }
-    });
-  }
+                return newPackage;
+            }
+        });
+    }
 
-  function process(filename) {
-    var style = postcss().process(fs.readFileSync(filename, 'utf8'));
+    function process(filename) {
+        var style = postcss().process(fs.readFileSync(filename, 'utf8'));
 
-    // recurse into each import. because we make the recursive call before
-    // extracting values (depth-first, post-order traversal), files that import
-    // libraries can re-define variable declarations, which more-closely
-    // matches the browser's behavior
-    style.root.eachAtRule(function(atRule) {
-      if (atRule.name !== 'import')
-        return;
+        // recurse into each import. because we make the recursive call before
+        // extracting values (depth-first, post-order traversal), files that import
+        // libraries can re-define variable declarations, which more-closely
+        // matches the browser's behavior
+        style.root.eachAtRule(function(atRule) {
+            if (atRule.name !== 'import')
+                return;
 
-      var stripped = atRule.params.replace(/^["']/, '').replace(/['"]$/, '');
-
-      process(resolveImport(stripped, dirname(filename)));
-    });
-
-
-    // extract variable definitions
-    style.root.eachRule(function(rule) {
-      if (rule.type !== 'rule')
-        return;
-
-      // only variables declared for `:root` are supported for now
-      if (rule.selectors.length !== 1 ||
-          rule.selectors[0] !== ':root' ||
-          rule.parent.type !== 'root')
-        return;
-
-      rule.each(function(decl) {
-        var prop = decl.prop;
-        var value = decl.value;
-
-        if (prop && prop.indexOf('--') === 0)
-          map.vars[prop] = value;
-      });
-    });
+            var stripped = atRule.params.replace(/^["']/, '').replace(/['"]$/, '');
+            process(resolveImport(stripped, filename));
+        });
 
 
-    // extract custom media declarations
-    style.root.eachAtRule(function(atRule) {
-      if (atRule.name !== 'custom-media')
-        return;
+        // extract variable definitions
+        style.root.eachRule(function(rule) {
+            if (rule.type !== 'rule')
+                return;
 
-      var name = atRule.params.split(/\s+/, 1)[0];
-      var nameRe = new RegExp('^' + name + '\\s+');
-      var rest = atRule.params.replace(nameRe, '');
+            // only variables declared for `:root` are supported for now
+            if (rule.selectors.length !== 1 ||
+                rule.selectors[0] !== ':root' ||
+                rule.parent.type !== 'root')
+                return;
 
-      map.media[name] = rest;
-    });
-  }
+            rule.each(function(decl) {
+                var prop = decl.prop;
+                var value = decl.value;
 
-  process(filename);
+                if (prop && prop.indexOf('--') === 0)
+                    map.vars[prop] = value;
+            });
+        });
 
-  return map;
+
+        // extract custom media declarations
+        style.root.eachAtRule(function(atRule) {
+            if (atRule.name !== 'custom-media')
+                return;
+
+            var name = atRule.params.split(/\s+/, 1)[0];
+            var nameRe = new RegExp('^' + name + '\\s+');
+            var rest = atRule.params.replace(nameRe, '');
+
+            map.media[name] = rest;
+        });
+    }
+
+    process(path_.resolve(filename));
+
+    return map;
 }
 
 /**
@@ -118,19 +117,19 @@ function makeVarMap(filename) {
  * [url-to-req]: https://github.com/webpack/css-loader/blob/7b50d4f569adcaf5bf185180c15435bde03f4de7/index.js#L37
  */
 function prependTildesToImports(styles) {
-  styles.eachAtRule(function(atRule) {
-    if (atRule.name !== 'import')
-      return;
+    styles.eachAtRule(function(atRule) {
+        if (atRule.name !== 'import')
+            return;
 
-    var stripped = atRule.params.replace(/^["']/, '').replace(/['"]$/, '');
+        var stripped = atRule.params.replace(/^["']/, '').replace(/['"]$/, '');
 
-    if (stripped[0] !== '.' && stripped[0] !== '~' && stripped[0] !== '/')
-      atRule.params = '"~' + stripped + '"';
+        if (stripped[0] !== '.' && stripped[0] !== '~' && stripped[0] !== '/')
+            atRule.params = '"~' + stripped + '"';
 
-  });
+    });
 }
 
 module.exports = {
-  makeVarMap: makeVarMap,
-  prependTildesToImports: prependTildesToImports
+    makeVarMap: makeVarMap,
+    prependTildesToImports: prependTildesToImports
 };
